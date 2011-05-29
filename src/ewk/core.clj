ec399874a1a20b0b25db974ab8cd509d3e0c2d82
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-29 15:21:59 -0500

    Adding the basic function to process an html file and attach feature results. Included some dummy features.

--- src/ewk/core.clj
+++ src/ewk/core.clj
@@ -2,9 +2,19 @@
   (:require [clojure.contrib.logging :as lg])
   (:require [clojure.contrib.string  :as string])
   (:require [net.cgrand.enlive-html  :as html])
+  (:require [ewk.features])
   (:use clojure.pprint)
   (:use clojure.contrib.json)
   (:gen-class))
 
 (defn -main [& args]
   (lg/info (str "Running with args: " (string/join " " args))))
+
+(defn add-features-to-dataset
+  [html]
+  (let [features (ns-publics 'ewk.features)]
+    (hash-map :html html
+              :features (apply hash-map
+                               (interleave
+                                 (map keyword (keys features))
+                                 (map #(% html) (vals features)))))))
---
+++ src/ewk/features.clj
@@ -0,0 +1,10 @@
+(ns ewk.features
+  (:use [net.cgrand.enlive-html]))
+
+(defn is-home
+  [html]
+  true)
+
+(defn header-exists
+  [html]
+  false)
