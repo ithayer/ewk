4001635a652f3da095c8e1c11666034f19acef1f
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-29 15:45:15 -0500

    Adding some real feature functions.

--- src/ewk/features.clj
+++ src/ewk/features.clj
@@ -2,9 +2,14 @@
   (:use [net.cgrand.enlive-html]))
 
 (defn is-home
+  "Check if the currentpage paragraph's content is Home"
   [html]
-  true)
+  ((complement empty?)
+     (filter #(= "Home" (apply str (% :content)))
+             (select html [:p#currentpage]))))
 
 (defn header-exists
+  "Check if there is a div with id header"
   [html]
-  false)
+  ((complement empty?)
+     (select html [:div.header])))
